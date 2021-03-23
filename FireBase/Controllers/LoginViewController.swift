d1c61effe0014379438783a345e836f0d3b54e4c
EreskinaAI <79778217151@mail.ru>
2021-03-24 01:40:13 +0300

    2d screen with tableView

---
+++ FireBase/Controllers/LoginViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  FireBase
+//
+//  Created by Анна Ереськина on 23.03.2021.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+    
+    @IBOutlet weak var warnLabel: UILabel!
+    @IBOutlet weak var emailTF: UITextField!
+    @IBOutlet weak var passwordTF: UITextField!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    @IBAction func loginTapped(_ sender: UIButton) {
+    }
+    @IBAction func registerTapped(_ sender: UIButton) {
+    }
+    
+
+}
+
